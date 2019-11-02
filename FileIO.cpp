c8ccfb8b6135691d0a82a6bac91a749fd0a4754a
Elijah Weske <54817873+ElijahWeske@users.noreply.github.com>
2019-11-02 18:29:37 -0400

    Update FileIO.cpp

--- FileIO.cpp
+++ FileIO.cpp
@@ -2,23 +2,55 @@
  * FileReader.cpp
  *
  *  Created on: Oct 8, 2017
- *      Author: keith
+ *      Author: Elijah Weske
  */
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include "../327_proj3_test/includes/FileIO.h"
 #include "../327_proj3_test/includes/constants.h"
 
 using namespace std;
+using namespace KP_FileIO;
 
-int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
-{
-	//TODO fill in
+//tries to open filename, and return its contents in contents
+//it will strip out only \n's (char return\line feed)
+//returns:
+//SUCCESS all worked, contents string contains all in the file
+//COULD_NOT_OPEN_FILE_TO_READ contents string is cleared
+int KP_FileIO::getFileContents(const std::string &filename, std::string &contents) {
+	ifstream inputFile;
+	string line;
+
+	inputFile.open(filename);
+	if (!inputFile.is_open()){
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+
+	// No built-in string parsing allowed...
+	while(!inputFile.eof()){
+		cout << line << endl;
+	}
+
+	inputFile.close();
+
 }
 
-int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
-{
-	//TODO fill in
+//tries to open filename, and serialize  myEntryVector contents to it
+//returns:
+//SUCCESS all worked, filename contains myEntryVector strings
+//COULD_NOT_OPEN_FILE_TO_WRITE
+int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector){
+	ofstream outputFile;
+	outputFile.open(filename);
+
+	if (!file.is_open()){
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+	}
+
+	for (int i = 0; i < myEntryVector.size(); i++){
+		file << myEntryVector[i] << endl;
+	}
+
+	return SUCCESS;
 }
-
-
